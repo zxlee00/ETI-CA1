71913ea64268b1336cf20c148a33ea4e8c7fdf83
Lee Zhi Xuan <zxnlee00@gmail.com>
2019-11-13 18:52:33 +0800

    modified home and blog pages to include page title added new test cases added picture of test outputs

--- Test Cases/test_cases.py
+++ Test Cases/test_cases.py
@@ -24,24 +24,13 @@ def test_scrolling():
     driver.maximize_window()
     driver.get("http://localhost:8000/projects")
 
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    scrollingHeight = driver.execute_script("return document.body.scrollHeight;")
+    driver.execute_script("window.scrollTo(0, " + str(scrollingHeight) + ");")
 
     scrollTop = driver.execute_script("return document.body.scrollTop;")
     scrollHeight = driver.execute_script("return document.body.scrollHeight;")
     clientHeight = driver.execute_script("return document.body.clientHeight;")
     assert scrollTop == scrollHeight - clientHeight
-
-    driver.quit()
-
-def test_redirection():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("http://localhost:8000/projects")
-
-    blog_link_elem = driver.find_element_by_link_text("Blog")
-    blog_link_elem.click()
-
-    assert WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "blogpageheader")))
 
     driver.quit()
 
@@ -58,6 +47,29 @@ def test_viewing_blog_page():
     assert driver.find_element_by_link_text("CCA")
     assert driver.find_element_by_link_text("Hobbies")
     assert driver.find_element_by_link_text("Taspense")
+
+    driver.quit()
+
+def test_check_home_page_title():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("http://localhost:8000/projects")
+
+    assert driver.title == "Home" 
+
+    driver.quit()
+
+def test_check_blog_page_title():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("http://localhost:8000/projects")
+
+    blog_link_elem = driver.find_element_by_link_text("Blog")
+    blog_link_elem.click()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "blogpageheader")))
+
+    assert driver.title == "Blog"
 
     driver.quit()
 
@@ -129,7 +141,6 @@ def test_leaving_a_comment():
     submit_btn_elem = driver.find_element_by_class_name("btn.btn-primary")
     submit_btn_elem.click()
 
-    ##do i need to verify that the comment is added? because after the comment is added once, if i just verify by text on the page, it could be left by prev comments
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//h1[text()[contains(., 'Taspense')]]"))) #wait for the page to refresh
     assert driver.find_element_by_xpath("//b[text()[contains(., 'Lee Zhi Xuan')]]")
     assert driver.find_element_by_xpath("//p[text()[contains(., 'Test Comment 1')]]")
